6f2b7bc73790005927e5a1b5e94cd0334983e7a0
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-10-16 01:25:30 -0400

    you don't need to be sudo to pull image

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker pull frank1chude1qian/dlbt_3090:latest
+docker pull frank1chude1qian/dlbt_3090:latest
 docker tag frank1chude1qian/dlbt_3090 gimel12/dlbt_public
 chmod +x dlbt_launcher
 ./dlbt_launcher
